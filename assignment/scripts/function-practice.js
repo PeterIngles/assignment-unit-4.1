26cb4fc84f2057823a8ad7efe2b93a4b191466c6
Peter Ingles <peter.ingles@gmail.com>
2023-07-02 12:18:56 -0500

    Finsished 1-7, Assignment 4.1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,30 +15,34 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
-
+console.log('Name Test - should say "Hello, Peter!"', helloName('Peter'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber
   // return firstNumber + secondNumber;
 }
+console.log('Add Numbers Test - should say 5 + 9=', addNumbers(5, 9))
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(firstNumber, secondNumber, thirdNumber) {
+  return firstNumber * secondNumber * thirdNumber;
 }
-
+console.log('Multiply Three Numbers Test - should say 5*9*2=', multiplyThree(5, 9, 2));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true
   }
-  return;
+  else if (number <= 0){
+    return false
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -50,15 +54,32 @@ console.log('isPositive - should say false', isPositive(-3));
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
+  if (array.length === 0){
+    return undefined
+  }
 
+  return array [array.length - 1];
 }
+
+console.log('Should say snake', getLast(['Cat', 'Dog', 'Snake']));
+console.log('Should say 3.14159', getLast([5000, 43, 3.14159]));
+console.log('Should say undefined', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log('Should say true', find ('Snake', ['Cat', 'Dog', 'Snake']));
+console.log('Should say true', find(43, [5000, 43, 3.14159]));
+console.log('Should say false', find(true, [false, false, false, false]));
 
 // ----------------------
 // Stretch Goals
